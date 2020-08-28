69d89f40f61ac8f207b7c2ad07fc3f1711e61341
pruebasonar-01 <68568995+pruebasonar-01@users.noreply.github.com>
2020-08-27 23:07:03 -0500

    Update GreenCalculator.java

--- src/main/java/com/restcalculator/logic/GreenCalculator.java
+++ src/main/java/com/restcalculator/logic/GreenCalculator.java
@@ -7,23 +7,7 @@ public class GreenCalculator {
 	public long number1;
 	public long number2;
 	
-	public long number3;
-	public long number4;
 	
-	public long number5;
-	public long number6;
-	
-	public long number7;
-	public long number8;
-	
-	public long number9;
-	public long number10;
-	
-	public long number7;
-	public long number8;
-	
-	public long number9;
-	public long number10;
 	
 	public GreenCalculator()
 	{
